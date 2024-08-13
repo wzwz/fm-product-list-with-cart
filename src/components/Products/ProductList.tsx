0c9d545d0cc8c27456962504b39809dbc389249d
Wei Zhong <ngwz92@gmail.com>
2024-08-13 23:41:19 +0800

    api url path fix

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -8,7 +8,7 @@ export default function ProductList() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/data.json");
+        const response = await fetch(import.meta.env.BASE_URL + "/data.json");
         const jsonData = await response.json();
         setProducts(jsonData);
       } catch (error) {
